fec3d56076488b3eedb982455c4022fc4b9d56b0
sistemassaitempsa <sistemassaitempsa@saitempsa.com>
2025-01-23 09:02:24 -0500

    se añade animacion al registro de candidatos

--- src/components/RegistroCandidatos.vue
+++ src/components/RegistroCandidatos.vue
@@ -1,187 +1,195 @@
 <template>
-  <div class="cardRegister animate__animated animate__fadeInUpBig">
-    <div class="principalContainerRegister">
-      <div class="logo logoRegister">
-        <img src="@/assets/logo1.png" alt="" />
+  <transition name="fadeInUpBig" appear>
+    <div class="cardRegister">
+      <div class="principalContainerRegister">
+        <div class="logo logoRegister">
+          <img src="@/assets/logo1.png" alt="" />
+        </div>
+        <form class="formRegister" action="">
+          <h2>Registro candidatos</h2>
+          <div class="row">
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                v-model="nombre"
+                class="form-control"
+                @focus="isFocusNombre = true"
+                @blur="isFocusNombre = false"
+                required
+              />
+              <label :class="{ active: isFocusNombre || nombre }"
+                >Nombres *</label
+              >
+            </div>
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="apellidos"
+                @focus="isFocusApellidos = true"
+                @blur="isFocusApellidos = false"
+                required
+              />
+              <label :class="{ active: isFocusApellidos || apellidos }"
+                >Apellidos *</label
+              >
+            </div>
+          </div>
+          <div class="row">
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="numero_documento"
+                @focus="isFocusNumDoc = true"
+                @blur="isFocusNumDoc = false"
+                required
+              />
+              <label :class="{ active: isFocusNumDoc || numero_documento }"
+                >Número de documento *</label
+              >
+            </div>
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="numero_documento_confirmation"
+                @focus="isFocusNumDocConfirmation = true"
+                @blur="isFocusNumDocConfirmation = false"
+                required
+              />
+              <label
+                :class="{
+                  active:
+                    isFocusNumDocConfirmation || numero_documento_confirmation,
+                }"
+                >Confirme número de documento *</label
+              >
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <select class="form-select" name="" id="">
+                <option value="">Tipo de documento *</option>
+              </select>
+            </div>
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="telefono"
+                @focus="isFocusTel = true"
+                @blur="isFocusTel = false"
+              />
+              <label
+                :class="{
+                  active: isFocusTel || telefono,
+                }"
+                >Teléfono *</label
+              >
+            </div>
+          </div>
+          <div class="row">
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="email"
+                @focus="isFocusEmail = true"
+                @blur="isFocusEmail = false"
+                required
+              />
+              <label
+                :class="{
+                  active: isFocusEmail || email,
+                }"
+                >Correo electrónico *</label
+              >
+            </div>
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="email_confirmation"
+                @focus="isFocusEmailConfirmation = true"
+                @blur="isFocusEmailConfirmation = false"
+                required
+              />
+              <label
+                :class="{
+                  active: isFocusEmailConfirmation || email_confirmation,
+                }"
+                >Confirme correo electrónico *</label
+              >
+            </div>
+          </div>
+          <div class="row">
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="password"
+                @focus="isFocusPassword = true"
+                @blur="isFocusPassword = false"
+                required
+              />
+              <label
+                :class="{
+                  active: isFocusPassword || password,
+                }"
+                >Contraseña *</label
+              >
+            </div>
+            <div class="col floating-label-group">
+              <input
+                type="text"
+                class="form-control"
+                v-model="password_confirmation"
+                @focus="isFocusPasswordConfirmation = true"
+                @blur="isFocusPasswordConfirmation = false"
+                required
+              />
+              <label
+                :class="{
+                  active: isFocusPasswordConfirmation || password_confirmation,
+                }"
+                >Confirme contraseña *</label
+              >
+            </div>
+          </div>
+          <div class="row mb-4">
+            <div class="col">
+              <button class="btn btn-success" @click="getTiposDocumento">
+                Registrarse
+              </button>
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col">
+              <label for=""
+                >¿Ya se encuentra registrado?
+                <span
+                  @click="emitLoginToggle"
+                  for="exampleInputPassword1"
+                  class="spanRegister"
+                >
+                  Ingrese aquí
+                </span></label
+              >
+            </div>
+          </div>
+        </form>
       </div>
-      <form class="formRegister" action="">
-        <h2>Registro candidatos</h2>
-        <div class="row">
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              v-model="nombre"
-              class="form-control"
-              @focus="isFocusNombre = true"
-              @blur="isFocusNombre = false"
-              required
-            />
-            <label :class="{ active: isFocusNombre || nombre }"
-              >Nombres *</label
-            >
-          </div>
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              class="form-control"
-              v-model="apellidos"
-              @focus="isFocusApellidos = true"
-              @blur="isFocusApellidos = false"
-              required
-            />
-            <label :class="{ active: isFocusApellidos || apellidos }"
-              >Apellidos *</label
-            >
-          </div>
-        </div>
-        <div class="row">
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              class="form-control"
-              v-model="numero_documento"
-              @focus="isFocusNumDoc = true"
-              @blur="isFocusNumDoc = false"
-              required
-            />
-            <label :class="{ active: isFocusNumDoc || numero_documento }"
-              >Número de documento *</label
-            >
-          </div>
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              class="form-control"
-              v-model="numero_documento_confirmation"
-              @focus="isFocusNumDocConfirmation = true"
-              @blur="isFocusNumDocConfirmation = false"
-              required
-            />
-            <label
-              :class="{
-                active:
-                  isFocusNumDocConfirmation || numero_documento_confirmation,
-              }"
-              >Confirme número de documento *</label
-            >
-          </div>
-        </div>
-        <div class="row">
-          <div class="col">
-            <select class="form-select" name="" id="">
-              <option value="">Tipo de documento *</option>
-            </select>
-          </div>
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              class="form-control"
-              v-model="telefono"
-              @focus="isFocusTel = true"
-              @blur="isFocusTel = false"
-            />
-            <label
-              :class="{
-                active: isFocusTel || telefono,
-              }"
-              >Teléfono *</label
-            >
-          </div>
-        </div>
-        <div class="row">
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              class="form-control"
-              v-model="email"
-              @focus="isFocusEmail = true"
-              @blur="isFocusEmail = false"
-              required
-            />
-            <label
-              :class="{
-                active: isFocusEmail || email,
-              }"
-              >Correo electrónico *</label
-            >
-          </div>
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              class="form-control"
-              v-model="email_confirmation"
-              @focus="isFocusEmailConfirmation = true"
-              @blur="isFocusEmailConfirmation = false"
-              required
-            />
-            <label
-              :class="{
-                active: isFocusEmailConfirmation || email_confirmation,
-              }"
-              >Confirme correo electrónico *</label
-            >
-          </div>
-        </div>
-        <div class="row">
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              class="form-control"
-              v-model="password"
-              @focus="isFocusPassword = true"
-              @blur="isFocusPassword = false"
-              required
-            />
-            <label
-              :class="{
-                active: isFocusPassword || password,
-              }"
-              >Contraseña *</label
-            >
-          </div>
-          <div class="col floating-label-group">
-            <input
-              type="text"
-              class="form-control"
-              v-model="password_confirmation"
-              @focus="isFocusPasswordConfirmation = true"
-              @blur="isFocusPasswordConfirmation = false"
-              required
-            />
-            <label
-              :class="{
-                active: isFocusPasswordConfirmation || password_confirmation,
-              }"
-              >Confirme contraseña *</label
-            >
-          </div>
-        </div>
-        <div class="row mb-4">
-          <div class="col">
-            <button class="btn btn-success" @click="getTiposDocumento">
-              Registrarse
-            </button>
-          </div>
-        </div>
-        <div class="row mb-2">
-          <div class="col">
-            <label for=""
-              >¿Ya se encuentra registrado?
-              <span for="exampleInputPassword1" class="spanRegister">
-                Ingrese aquí
-              </span></label
-            >
-          </div>
-        </div>
-      </form>
     </div>
-  </div>
+  </transition>
 </template>
-
 <script setup>
+import { defineEmits } from "vue";
 import { ref } from "vue";
-/* import SearchList from "./SearchList.vue"; */
-
+const emit = defineEmits(["toogleRegisterChild"]);
+const emitLoginToggle = () => {
+  emit("toogleRegisterChild");
+};
 const nombre = ref("");
 const apellidos = ref("");
 const numero_documento = ref("");
@@ -195,18 +203,14 @@ const isFocusEmail = ref(false);
 const isFocusEmailConfirmation = ref(false);
 const isFocusPassword = ref(false);
 const isFocusPasswordConfirmation = ref(false);
-
-/* const consulta_tip_doc = ref(""); */
-/* const tip_doc_id = ref(""); */
 const email = ref("");
 const email_confirmation = ref("");
 const password = ref("");
 const password_confirmation = ref("");
 const telefono = ref("");
-/* const tipos_doc_array = ref([]); */
-
 const getTiposDocumento = async () => {};
 </script>
+
 <style scoped>
 .floating-label-group {
   position: relative;
@@ -251,8 +255,8 @@ const getTiposDocumento = async () => {};
   position: absolute;
   z-index: 100;
   left: 15%;
-  animation: fadeInUpBig; /* referring directly to the animation's @keyframe declaration */
-  animation-duration: 1s;
+  /* animation: fadeInUpBig; 
+  animation-duration: 1s; */
   border-radius: 5px;
 }
 
@@ -288,5 +292,11 @@ label {
 }
 .formRegister {
   width: 60%;
+}
+.fadeInUpBig-enter-active {
+  animation: fadeInUpBig 1s; /* Animate.css: animación de entrada */
+}
+.fadeInUpBig-leave-active {
+  animation: fadeOutDownBig 1s; /* Animate.css: animación de salida */
 }
 </style>
