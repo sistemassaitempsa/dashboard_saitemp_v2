bcc7633fa331afea30cf4aea3ce3951298110c98
sistemassaitempsa <sistemassaitempsa@saitempsa.com>
2025-05-07 16:12:48 -0500

    se hace cambio para la carga de documentos pdf

--- src/components/LovePdfToJpg.vue
+++ src/components/LovePdfToJpg.vue
@@ -90,7 +90,15 @@
         </div>
       </div>
       <div class="pages-container">
-        <div v-if="loading" class="loading">Cargando PDF...</div>
+        <div v-if="loading">
+          <div class="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <h5>Cargando PDF...</h5>
+        </div>
         <div class="input_drop_container" v-if="multipleFiles.length == 0">
           <div class="legend_drop_container">
             <svg
@@ -722,5 +730,48 @@ button:hover {
   height: 76%;
   margin-top: 0;
   color: rgba(192, 192, 192, 0.637);
+}
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  margin-top: 50px;
+  color: red;
+}
+
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid rgb(10, 10, 10);
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: rgb(199, 195, 195) transparent transparent transparent;
+}
+
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
